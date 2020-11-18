723857bb59891ae7bcaa29dc11f29a46b1f91349
Julian Esteban Rios Holguin <desarrolladorjr4.spv@gmail.com>
2020-11-17 22:54:25 -0500

    Initial commit: Feedback App - Backend Backend in Net Core 3.1 to register data in MYSQL

---
+++ comment-back/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using comment_back.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace comment_back
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public DbSet<Comment> Comment { get; set; }
+
+        public ApplicationDbContext()
+        {
+
+        }
+
+        public ApplicationDbContext(DbContextOptions options): base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            if (!optionBuilder.IsConfigured)
+            {
+                optionBuilder.UseMySql("Server=localhost;Database=Comments;Uid=root;Pwd=");
+            }
+        }
+    }
+}
---
+++ comment-back/Models/Comment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace comment_back.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Creator { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public DateTime DateCreation { get; set; }
+    }
+}
